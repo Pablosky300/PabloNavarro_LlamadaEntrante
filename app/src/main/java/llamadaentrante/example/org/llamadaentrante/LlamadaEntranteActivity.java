f01514157ad0d88e08abfc52c9ae19666acff23a
DAM2 <pablo160896@gmail.com>
2017-02-02 17:13:10 +0100

    Initial commit

---
+++ app/src/main/java/llamadaentrante/example/org/llamadaentrante/LlamadaEntranteActivity.java
@@ -0,0 +1,13 @@
+package llamadaentrante.example.org.llamadaentrante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LlamadaEntranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llamada_entrante);
+    }
+}
